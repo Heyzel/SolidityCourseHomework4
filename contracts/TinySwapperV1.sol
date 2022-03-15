e0e83d3f5b04acea026c50304a0e4fc685ff6183
Heyzel <heyzelm7@gmail.com>
2022-03-15 12:21:06 -0400

    added an event and comments

--- contracts/TinySwapperV1.sol
+++ contracts/TinySwapperV1.sol
@@ -7,9 +7,19 @@ import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
 contract TinySwapperV1 is OwnableUpgradeable  {
     using SafeMath for uint256;
+
+    /**
+    * @notice Event to notify the tokens received for the amount given
+    */
+    event SwapSuccess(uint amountGiven, uint amountReceived, address tokenReceived);
     
-    address public feeRecipient;
+    // ========== VARIABLES V1 ========== //
+
+    // fee charged, initialized in 0.1%
     uint16 public fee;
+
+    // Receives `fee` of the total ETH used for swaps   
+    address public feeRecipient;
     ISwapRouter public constant swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
     address private constant WETH9 = 0xd0A1E359811322d97991E03f863a0C30C2cF029C;
 
@@ -20,10 +30,18 @@ contract TinySwapperV1 is OwnableUpgradeable  {
         fee = _fee;
     }
 
+    /**
+    * @notice swap ETH for multiple tokens according to percentages
+    * @dev the total of msg.value will be converted to tokens
+    * @dev `_tokens` and `_percentages` must have same length
+    * @param _tokens array of tokens to swap
+    * @param _percentages array of % amount to convert eth from with two decimals (e.g. 9999 = 99.99%)
+    */
     function swap(address[] memory _tokens, uint[] memory _percentages) external payable {
         require(msg.value > 0, "Insufficient ETH");
         require(_tokens.length == _percentages.length, "tokens and percentages does not have the same size");
 
+        // Calculate ETH to make the swaps
         uint moneyToSwap = msg.value.sub(msg.value.mul(fee).div(10000));
         uint amount; uint amountOut;
 
@@ -42,9 +60,10 @@ contract TinySwapperV1 is OwnableUpgradeable  {
             });
 
             amountOut = swapRouter.exactInputSingle{value: amount}(params);
-
+            emit SwapSuccess(amount, amountOut, _tokens[i]);
         }
 
+        // Send ETH remaining to fee recipient
         (bool success, ) = feeRecipient.call{ value: (address(this).balance) }("");
         require(success, "refund failed");
     }
